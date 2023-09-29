cd8927815af65081369a4bcb7397d951559c0467
“javascriptseo” <seofreelance2021@outlook.com>
2023-09-28 19:15:07 -0700

    first commit

--- main.py
+++ main.py
@@ -15,10 +15,17 @@ NUM_LOOPS = 10  # Constant used to control loop.
 print("0 through 10 multiplied by 2 and by 10" + "\n")
 
 # Initialize loop control variable.
+numberCounter = 0
+
 # Write your counter controlled while loop here
+while numberCounter <= NUM_LOOPS:
     # Multiply by 10.
+    byTen = numberCounter * 10
     # Multiply by 2.
+    byTen = numberCounter * 2
+
     print(head1 + str(numberCounter))
     print(head2 + str(byTwo))
     print(head3 + str(byTen))
     # Next number.
+    numberCounter = numberCounter + 1
